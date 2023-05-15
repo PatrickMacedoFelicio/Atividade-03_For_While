977c372f7a17f7b619a95382d300a8bd22ecf712
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-05-15 11:36:53 -0400

    Update Numero até

---
+++ Atv04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.Write("Informe um valor inicial:\n>> ");
+            int numIn = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe um valor final:\n>> ");
+            int numFim = Convert.ToInt32(Console.ReadLine());
+
+            while(numFim > 0)
+            {
+
+            }
+        }
+    }
+}
