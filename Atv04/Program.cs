f9b9ac691abcbbd2b986a479a63067954ae97ba4
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-05-16 10:23:08 -0400

    Update 4

--- Atv04/Program.cs
+++ Atv04/Program.cs
@@ -10,16 +10,37 @@ namespace Atv04
     {
         static void Main(string[] args)
         {
+            int intPosi = 0, numPar = 0, numImpar = 0, div3 = 0, div7 = 0, count = 0, soma = 0;
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
             Console.Write("Informe um valor inicial:\n>> ");
             int numIn = Convert.ToInt32(Console.ReadLine());
             Console.Write("Informe um valor final:\n>> ");
             int numFim = Convert.ToInt32(Console.ReadLine());
 
-            while(numFim > 0)
+            while(numIn <= numFim)
             {
+                count++;
+                // Par e Impar
+                if (numIn % 2 == 0)
+                    numPar++;
+                else
+                    numImpar++;
+                // Divisivel por 3 e 7
+                if (numIn % 3 == 0)
+                    div3++;
+                if (numIn % 7 == 0)
+                    div7++;
 
+                soma = soma + numIn;
+                numIn++;
             }
+            Console.WriteLine("\n-=-=-=-=-=-=-=-=-=- Resultados -=-=-=-=-=-=-=-=-=-");
+            Console.WriteLine("Tivemos "+ numPar + " números Pares.");
+            Console.WriteLine("Tivemos "+ numImpar + " números Impares.");
+            Console.WriteLine(div3+" Números divisiveis por 3.");
+            Console.WriteLine(div7+" Números divisiveis por 7.");
+            Console.WriteLine("Somando todos os números o resultado é "+soma+", é a média é: "+(soma)/count);
+            Console.ReadKey();
         }
     }
 }
