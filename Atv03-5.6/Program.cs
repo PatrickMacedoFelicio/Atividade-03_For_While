b57e009859e02d2e963719d780dff759d8d8bdba
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-05-20 10:56:06 -0400

    atv 3 5.6 update

--- Atv03-5.6/Program.cs
+++ Atv03-5.6/Program.cs
@@ -10,6 +10,19 @@ namespace Atv03_5._6
     {
         static void Main(string[] args)
         {
+            int num, fator;
+            Console.WriteLine("-=-=-=-=-=-=-=- Fatorial -=-=-=-=-=-=-");
+            Console.Write("-- Digite um número para fazer o fatorial\n>> ");
+            num = int.Parse(Console.ReadLine());
+            fator = num;
+            for (int i = num - 1; i >= 1; i--)
+            {
+                fator = fator * i;
+            }
+            Console.Clear();
+            Console.WriteLine("\n-=-=-=-=-=-=-=- Resultado FAtorial -=-=-=-=-=-=-=-");
+            Console.WriteLine($"O fatorial de {num}! é {fator}");
+            Console.ReadKey();
         }
     }
 }
