e195b3b8c962a11cfa0a4ad0764410e36119d28e
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-05-20 11:50:04 -0400

    atv 4 5.6

---
+++ Atv04-5.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv04_5._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-=-=-=-=-=- Potenciação -=-=-=-=-=-");
+            Console.Write("Digite o número base para a ponteciação:\n>> ");
+            int numBase = int.Parse(Console.ReadLine());
+            Console.Write("Digite o expoente para a ponteciação:\n>> ");
+            int expoente = int.Parse(Console.ReadLine());
+            int resultado = numBase;
+            do
+            {
+                resultado = resultado * numBase;
+                expoente--;
+            } while(expoente != 1);
+            Console.WriteLine($"\n-- O resultado da Potencia é: {resultado}");
+            Console.ReadKey();
+        }
+    }
+}
