42cb946b5db96bf3140c6325aefd89b123e78174
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-05-22 10:48:00 -0400

    Atv6 5.6

---
+++ Atv6-5.6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv6_5._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string situacao;
+            do
+            {
+                int primo = 0;
+                Console.WriteLine("-=-=-=-=-=-=-=- Números primos -=-=-=-=-=-=-=-");
+                Console.Write("-- Digite um número para verificar se é um núemro Primo:\n>> ");
+                int num = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                        primo++;
+                }
+                if (primo == 2)
+                    Console.WriteLine($"\n-- O número {num} é um Número Primo!");
+                else
+                    Console.WriteLine($"\n --O número {num} não é um Número Primo!");
+                Console.Write("\n-- Deseja continuar as verificações?\n(S/N) >> ");
+                situacao = Console.ReadLine();
+                situacao = situacao.ToUpper();
+                Console.Clear();
+            } while (situacao == "S");
+            Console.Clear();
+            Console.WriteLine("-- Obrigado por participar!");
+            Console.WriteLine("Digite qualquer coisa para sair...");
+            Console.ReadKey();
+        }
+    }
+}
