9a2faa808b53cdf7d774b4ed7c5bd5a022bee6f2
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-05-25 10:25:31 -0400

    create 9 5.6

---
+++ Atv9-5.6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv9_5._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
