88a28fa74b30dfd3b9c00eb2eba5f1c8cfa8d825
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-05-28 16:26:14 -0400

    Update 9 5.6

--- 5.6-Atv09/Program.cs
+++ 5.6-Atv09/Program.cs
@@ -10,10 +10,20 @@ namespace Atv9_5._6
     {
         static void Main(string[] args)
         {
+            int veloM = 80;
+            int veloN = 100;
             Console.WriteLine("-=-=-=-=-=-=-=-=- Encontro de Trens -=-=-=-=-=-=-=-");
-            Console.Write("-- Qual é a distancia entre a cidade A e cidade B?\n>>  ");
-            double distancia = double.Parse(Console.ReadLine());
+            Console.Write("-- Qual é a distancia entre a cidade A e cidade B? (em quilometros)\n>>  ");
+            int distancia = int.Parse(Console.ReadLine());
 
+            int tempEncontro = (distancia * 60) / (veloM + veloN);
+            int quilomM = (veloM * tempEncontro) / 60;
+            int quilomN = (veloN * tempEncontro) / 60;
+
+            Console.WriteLine($"O encontro dos trens ocorrerá em {tempEncontro} minutos.");
+            Console.WriteLine($"O trem M percorrerá {quilomM}km até o encontro.");
+            Console.WriteLine($"O trem N percorrerá {quilomN}km até o encontro.");
+            Console.ReadKey();
         }
     }
 }
